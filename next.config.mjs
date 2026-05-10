51865030be70eb2c4d0ae7814df980cbc14ed40b
nemgpSolutions <contact@nemgp.com>
2026-05-10 10:22:21 +0200

    Fix: workflow and config to ensure static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  basePath: '/nempSolutions', // Adjust if using a custom domain or different repo name
+  basePath: '/nempSolutions',
   images: {
     unoptimized: true,
     remotePatterns: [
